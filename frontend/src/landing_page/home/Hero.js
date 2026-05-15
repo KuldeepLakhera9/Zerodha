36b74255b969640be97f585a0e777689ecac4b16
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-05-15 18:02:59 +0530

    Setting up Home page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,9 +2,22 @@ import React from "react";
 
 function Hero() {
   return (
-    <h1>
-      Hero<i class="fa-solid fa-circle-user"></i>
-    </h1>
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img src="media/images/homeHero.png" alt="HeroImage" className="mb-5" />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+          bonds, and more.
+        </p>
+        <button
+          className="p-2 fs-5 mb-5 btn btn-primary"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Sign up for free
+        </button>
+      </div>
+    </div>
   );
 }
 
