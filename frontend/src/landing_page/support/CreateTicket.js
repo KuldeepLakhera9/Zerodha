5b7cdb6a25773b2ca7526ed007937cc7224c1786
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-05-17 12:55:42 +0530

    Complete with Support Component

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,7 +1,291 @@
 import React from "react";
 
 function CreateTicket() {
-  return <h1>CreateTicket</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className=" ">To create a ticket ,select a relevent topic</h1>
+        <div className="col p-5  mt-5">
+          {" "}
+          <h4 className="mb-5 ">
+            <i class="fa-solid fa-circle-plus"></i> Account Opening
+          </h4>
+          <a href="" style={{ textDecoration: "none" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Charges at Zerodha Zerodha IDFC FIRST
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Bank 3-in-1 Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Getting Started
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="col p-5 mb-5 mt-5">
+          {" "}
+          <h4 className=" mb-5">
+            <i class="fa-regular fa-user"></i> Your Zerodha Account
+          </h4>
+          <a href="" style={{ textDecoration: "none" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Charges at Zerodha Zerodha IDFC FIRST
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Bank 3-in-1 Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Getting Started
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="col p-5 mb-5 mt-5">
+          {" "}
+          <h4 className="mb-5">
+            <i class="fa-brands fa-accusoft"></i> Kite
+          </h4>
+          <a href="" style={{ textDecoration: "none" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Charges at Zerodha Zerodha IDFC FIRST
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Bank 3-in-1 Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Getting Started
+          </a>
+          <br />
+          <br />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col p-5 mb-5 mt-5">
+          {" "}
+          <h4 className="mb-5 ">
+            <i class="fa-solid fa-indian-rupee-sign"></i> Fund
+          </h4>
+          <a href="" style={{ textDecoration: "none" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Charges at Zerodha Zerodha IDFC FIRST
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Bank 3-in-1 Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Getting Started
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="col p-5 mb-5 mt-5">
+          {" "}
+          <h4 className=" mb-5">
+            <i class="fa-solid fa-copyright"></i> Console
+          </h4>
+          <a href="" style={{ textDecoration: "none" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Charges at Zerodha Zerodha IDFC FIRST
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Bank 3-in-1 Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Getting Started
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="col p-5 mb-5 mt-5">
+          {" "}
+          <h4 className="mb-5">
+            <i class="fa-regular fa-circle"></i> Coin
+          </h4>
+          <a href="" style={{ textDecoration: "none" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Charges at Zerodha Zerodha IDFC FIRST
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Bank 3-in-1 Account
+          </a>
+          <br />
+          <br />
+          <a href="" style={{ textDecoration: "none" }}>
+            Getting Started
+          </a>
+          <br />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
